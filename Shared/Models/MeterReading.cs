6dde64479f7e225dfee77d27b3a5c43750e72ccb
Admin <Admin@ADMINWI-JKQS8GG>
2024-09-30 20:22:29 +0100

    Added models

---
+++ Shared/Models/MeterReading.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Models
+{
+    public class MeterReading
+    {
+        public int MeterReadingId { get; set; }
+        public int AccountId { get; set; }
+        public DateTime MeterReadingDate { get; set; }
+        public int MeterReadingValue { get; set; }
+
+        public Account Account { get; set; }
+    }
+}
---
+++ Shared/Models/MeterReadingDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+    public class MeterReadingDto
+    {
+        public int AccountId { get; set; }
+        public DateTime MeterReadingDate { get; set; }
+        public int MaterReadingValue { get; set; }
+    }
+}
