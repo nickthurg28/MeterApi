41f3e7942671c3a9726859724b191921684362f0
Admin <Admin@ADMINWI-JKQS8GG>
2024-09-30 20:22:02 +0100

    Added datalayer

---
+++ DataLayer/MeterReadingDataBase.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
+
+namespace DataLayer
+{
+    public class MeterReadingDataBase : DbContext
+    {
+        public MeterReadingDataBase(DbContextOptions<MeterReadingDataBase> options) : base(options) { }
+
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<MeterReading> MeterReadings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>().HasKey(a => a.AccountId);
+            modelBuilder.Entity<MeterReading>().HasKey(m => m.MeterReadingId);
+
+            modelBuilder.Entity<MeterReading>()
+                .HasOne(m => m.Account)
+                .WithMany()
+                .HasForeignKey(m => m.AccountId);
+        }
+    }
+}
+
