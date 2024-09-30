49c16f154d869aaab31537aa65ab11487031ecdd
Admin <Admin@ADMINWI-JKQS8GG>
2024-09-30 13:36:33 +0100

    Removed default controller and added dummy endpoint for PoC

---
+++ Controllers/MeterReading.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MeterApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MeterReading : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("meter-reading-uploads")]
+        public async Task<IActionResult> DefaultEndpoint()
+        {
+            return Ok("Working");
+        }
+        
+        [HttpPost]
+        [Route("meter-reading-uploads")]
+        public async Task<IActionResult> UploadMeterReadings()
+        {
+            return Ok("Working");
+        }
+    }
+}
